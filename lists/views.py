bfaec268d038d5409919d3dc041a886e681219ab
piotr <piotrek_cypu1@wp.pl>
2016-01-14 12:54:53 +0100

    Seva item only if necessary

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 from django.http import HttpResponse
-# Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text', '')
+        item = Item()
+        item.text = new_item_text
+        item.save()
+        return redirect('/')
+    else:
+        new_item_text = ''
+
+    return render(request, 'home.html', {'new_item_text': new_item_text})
